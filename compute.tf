038271e00ae5b41705f158edfac2bd24c488667e
Franchise <tylerreedsullivan@gmail.com>
2021-06-22 09:48:54 -0500

    Added future state remote backend.

--- compute.tf
+++ compute.tf
@@ -1,4 +1,10 @@
 terraform {
+    # backend "remote" {
+    #     organization = "TheFranchise"
+    #     workspaces = {
+    #         name = "TheFranchiseWorkspace"
+    #     }
+    # }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
