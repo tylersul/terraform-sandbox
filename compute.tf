6484cb49ec33ec8e3a1bd83ff2ec2deb78169b8d
Franchise <tylerreedsullivan@gmail.com>
2021-06-22 08:27:58 -0500

    Initial commit to remote.

---
+++ compute.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.0.0"
+    }
+  }
+
+  required_version = "> 0.14"
+}
+
+provider "aws" {
+  profile = "iac-user-master"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "public-ec2" {
+  ami           = "ami-0aeeebd8d2ab47354"
+  instance_type = "t2.micro"
+  tags = {
+    Owner = "iac-user"
+  }
+}
