c7366b9f5e9bf8684766349e4b15cb62698158e1
Franchise <tylerreedsullivan@gmail.com>
2022-01-26 10:30:02 -0600

    Comments.

--- compute.tf
+++ compute.tf
@@ -1,3 +1,6 @@
+### Terraform Block ###
+# Contains Terraform settings, including the required providers 
+#    Terraform will use to provision your infrastructure.
 terraform {
   required_providers {
     aws = {
@@ -9,17 +12,25 @@ terraform {
   required_version = "> 0.14"
 }
 
+### Provider Block ###
+# Configures the specified provider, in this case AWS. 
+#   A provider is a plugin that Terraform uses to create and manage 
+#   your resources.
 provider "aws" {
   profile = "ts-master"
   region  = var.aws_region
 }
 
-resource "aws_vpc" "MainVPC" {
-  cidr_block = var.vpc_cidr_block
-}
-
+### Resource Block ###
+# Defines components of your infrastructure. 
+# A resource might be a physical or virtual component such as 
+# an EC2 instance, or it can be a logical resource such as a Heroku app
 resource "aws_instance" "MyEC2" {
   ami           = "ami-0aeeebd8d2ab47354"
   instance_type = "t2.micro"
   tags          = var.tags
+}
+
+resource "aws_vpc" "MainVPC" {
+  cidr_block = var.vpc_cidr_block
 }
